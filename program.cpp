c8fd6c27b27c7c2d002fb8f64e6db4bc0600af37
Angelina Lagunova <lagunova_gelya@mail.ru>
2025-01-06 00:52:09 +1000

    modified:   program.cpp 	modified:   translator.py

--- program.cpp
+++ program.cpp
@@ -1,2 +1,10 @@
 
+int main(){
     int a = 15;
+    float b = 15;
+    cout<<a;
+    int bb;
+    string str = "strg";
+    cout<<a;
+
+}
--- translator.py
+++ translator.py
@@ -189,25 +189,88 @@ class Parser:
         else:
             raise Exception(f'Expected token type {expected_type}')
 
+    def checkTokensList(self, tokenlist):
+        for i in tokenlist:
+            token = self.current_token()
+            if token.token != i:
+                return False
+            if self.position < len(self.tokens):  # если мы не в конце программы, то все ок, смещаемся дальше
+                self.position += 1
+            else:
+                return False
+        return True
+
     def parse_program(self):
         node = Node('Program')
         while self.position < len(self.tokens):
+            node.add_child(self.parse_main())
+        return node
+
+    def parse_main(self):
+        thistokenlist = ['K17', 'K32', 'D6', 'D7', 'D4']
+        if self.checkTokensList(thistokenlist):
+            node = Node('MainFunction')
+            node.add_child(self.parse_body())
+            token = self.current_token()
+            if token.token == 'D5':
+                self.consume('D5')
+                return node
+            else:
+                raise Exception('не закрыта фигурная скобка')
+        else:
+            raise Exception('ошибка объявления главной функции')
+
+    def parse_body(self):
+        node = Node('Body')
+        node.add_child(self.parse_code_block())
+        while self.position < len(self.tokens) - 1:
+            node.add_child(self.parse_code_block())
+        return node
+
+    def parse_code_block(self):
+        node = Node('Instruction')
+        token = self.current_token()
+        if token.token in ['K17', 'K18', 'K22']:
             node.add_child(self.parse_declaration())
+        elif token.token == 'K24':
+            node.add_child(self.parse_cout())
+        else:
+            raise Exception('ошибка в блоке кода')
         return node
 
     def parse_declaration(self):
         token = self.current_token()
-        if token.token in ['K17', 'K18', 'K22']:  # Включите другие типы по мере необходимости
+        if token.token in ['K17', 'K18', 'K22']:
             var_node = Node('VariableDeclaration')
             var_type = token.lexeme
             self.consume(token.token)
             var_node.add_child(Node('Type', var_type))
             var_node.add_child(self.parse_identifier())
-            self.consume('O23')  # '='
-            var_node.add_child(self.parse_expression())
-            self.consume('D3')  # ';'
+            token = self.current_token()
+            if token.token == 'O23':
+                self.consume('O23')  # '='
+                var_node.add_child(self.parse_expression())
+            token = self.current_token()
+            if token.token == 'D3':
+                self.consume('D3')  # ';'
+            else:
+                raise Exception('пропущена точка с запятой')
             return var_node
         raise Exception('Unexpected token')
+    
+    def parse_cout(self):
+        if self.tokens[self.position].token == 'K24':
+            self.consume('K24')
+            if self.tokens[self.position].token == 'O25':
+                self.consume('O25')
+                token = self.tokens[self.position]
+                if token.token == 'ID' or token.token == 'N1' or token.token == 'N2' or token.token == 'N3':
+                    self.consume(token.token)
+                    if self.tokens[self.position].token == 'D3':
+                        self.consume('D3')
+                        node = Node('Cout', token.lexeme)
+                        return node
+        raise Exception('Ошибка вывода')
 
     def parse_identifier(self):
         token = self.current_token()
@@ -216,16 +279,32 @@ class Parser:
             self.consume('ID')
             return id_node
         raise Exception('Expected identifier')
-    
+
     def parse_expression(self):
-        # Здесь вы можете реализовать допустимые выражения
         token = self.current_token()
         if token.token == 'N1':
             value_node = Node('Integer', token.lexeme)
             self.consume('N1')
             return value_node
-        raise Exception('Expected expression')
-    
+        elif token.token == 'N2':
+            value_node = Node('Float', token.lexeme)
+            self.consume('N2')
+            return value_node
+        elif token.token == 'N3':
+            value_node = Node('String', token.lexeme)
+            self.consume('N3')
+            return value_node
+        else:
+            raise Exception('Expected expression')
+
+    def print_syntax_tree(self, node, level=0):
+        """ Рекурсивно выводит синтаксическое дерево. """
+        if node is None:
+            return
+        print("\t" * level + f"{node.node_type}: {node.value if node.value is not None else ''}")
+        for child in node.children:
+            self.print_syntax_tree(child, level + 1)
+
 
 class CodeGenerator:
     def __init__(self):
@@ -256,34 +335,6 @@ class CodeGenerator:
     def get_code(self):
         return "\n".join(self.code)
 
-
-# class Syntax_Analyzer:
-#     debug = True
-
-#     def __init__(self, tokens):
-#         self.tokens = tokens
-
-#     def show(self, indent, name, spp):
-#         if self.debug:
-#             print(indent + name + '("', end=' ')
-#             j = len(self.tokens)
-#             for i in range(spp, j):
-#                 print(self.tokens[i], sep="", end=" ")
-#             print('")', end='\n')
-#             return
-#         else:
-#             return
-
-#     def checkTokensList(self, spp, tokenlist):
-#         for i in tokenlist:
-#             if self.tokens[spp] != i:
-#                 return False, spp
-#             if spp < len(self.tokens):
-#                 spp += 1
-#             else:
-#                 return False, spp
-#         return True, spp
-
 #     def prog(self, spp, indent):
 #         self.show(indent, 'prog', spp)
 #         indent = indent+' '
@@ -292,81 +343,6 @@ class CodeGenerator:
 #         if res:
 #             return self.mainFunc(spp, indent)
 #         return False, spp
-
-#     def mainFunc(self, spp, indent):
-#         self.show(indent, 'main', spp)
-#         indent = indent+' '
-#         # spp1 = spp
-#         thistokenlist = ['K17', 'K32', 'D6', 'D7', 'D4']
-#         res, spp = self.checkTokensList(spp, thistokenlist)
-#         if res:
-#             res, spp = self.body(spp, indent)
-#             return (tokens[spp] == 'D5'), spp
-#         return False, spp
-
-#     def body(self, spp, indent):
-#         self.show(indent, 'body', spp)
-#         indent = indent+' '
-#         spp1 = spp
-#         res, spp = self.codeBlock(spp, indent)
-#         if res:
-#             while tokens[spp] != 'D5' and res:
-#                 res, spp = self.codeBlock(spp, indent)
-#             return res, spp
-#         return False, spp1
-
-#     def codeBlock(self, spp, indent):
-#         # show(indent, 'codeBlock', spp)
-#         indent = indent+' '
-#         spp1 = spp
-#         res, spp = self.var(spp, indent)
-#         if res:
-#             return res, spp
-#         res, spp = self.outBlock(spp, indent)
-#         if res:
-#             return res, spp
-#         return False, spp1
-
-#     def outBlock(self, spp, indent):
-#         indent = indent+' '
-#         spp1 = spp
-#         if self.tokens[spp] == 'K24':
-#             if spp < len(self.tokens):
-#                 spp += 1
-#             if self.tokens[spp] == 'O25':
-#                 if spp < len(self.tokens):
-#                     spp += 1
-#                 if self.tokens[spp] == 'ID' or self.tokens[spp] == 'N1' or self.tokens[spp] == 'N2' or self.tokens[spp] == 'N3':
-#                     if spp < len(self.tokens):
-#                         spp += 1
-#                     if self.tokens[spp] == 'D3':
-#                         self.show(indent, 'outBlock', spp1)
-#                         return True, spp+1
-#         return False, spp1
-
-#     def var(self, spp, indent):
-
-#         indent = indent+' '
-#         spp1 = spp
-#         if self.tokens[spp] == 'K17' or self.tokens[spp] == 'K18' or self.tokens[spp] == 'K22':
-#             if spp < len(self.tokens):
-#                 spp += 1
-#             if self.tokens[spp] == 'ID':
-#                 if spp < len(self.tokens):
-#                     spp += 1
-#                 if self.tokens[spp] == 'D3':
-#                     self.show(indent, 'var', spp1)
-#                     return True, spp+1
-#                 elif self.tokens[spp] == 'O23':
-#                     if spp < len(self.tokens):
-#                         spp += 1
-#                     if self.tokens[spp] == 'N1' or self.tokens[spp] == 'N2' or self.tokens[spp] == 'N3':
-#                         if spp < len(self.tokens):
-#                             spp += 1
-#                         if self.tokens[spp] == 'D3':
-#                             self.show(indent, 'var', spp1)
-#                             return True, spp+1
-#         return False, spp1
 
 
 def preproc(pretokens):
@@ -417,6 +393,7 @@ if __name__ == '__main__':
 
     parser = Parser(tokens)
     program_node = parser.parse_program()  # Получаем дерево
+    parser.print_syntax_tree(program_node)
     # generator = CodeGenerator()
     # generator.generate(program_node)      
     # java_code = generator.get_code()       
